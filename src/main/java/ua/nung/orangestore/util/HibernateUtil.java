6857e55868733848df1259cd6bd5456e58b18bdd
Ihor Liutak <ihor.lyutak@gmail.com>
2026-02-15 18:55:45 +0000

    added hibernate

---
+++ src/main/java/ua/nung/orangestore/util/HibernateUtil.java
@@ -0,0 +1,36 @@
+package ua.nung.orangestore.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Properties properties = new Properties();
+            InputStream input = HibernateUtil.class
+                    .getClassLoader()
+                    .getResourceAsStream("project.properties");
+
+            properties.load(input);
+
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            configuration.addProperties(properties);
+
+            sessionFactory = configuration.buildSessionFactory();
+
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
